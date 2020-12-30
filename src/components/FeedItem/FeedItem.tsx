640cb6b6458b4c4a6f049048f5d0c026a35157a3
Nitheesh KR <nitheeshkr115@gmail.com>
2020-12-30 18:55:48 +0530

    updated card styles

--- src/components/FeedItem/FeedItem.tsx
+++ src/components/FeedItem/FeedItem.tsx
@@ -1,29 +1,11 @@
 import { Card, Button } from 'react-bootstrap';
 import { useHistory } from "react-router-dom";
-import { Col, Row, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
-
-const editTooltip = (props) => (
-    <Tooltip id="button-tooltip" {...props}>
-        Edit RSS Feed
-    </Tooltip>
-);
-
-const deleteTooltip = (props) => (
-    <Tooltip id="button-tooltip" {...props}>
-        Delete RSS Feed
-    </Tooltip>
-);
-
-const viewTooltip = (props) => (
-    <Tooltip id="button-tooltip" {...props}>
-        See content of RSS Feed.
-    </Tooltip>
-);
 
 function FeedItem({ feedData, editFeed, deleteFeed }) {
     let history = useHistory();
@@ -44,9 +26,9 @@ function FeedItem({ feedData, editFeed, deleteFeed }) {
         <Card className="shadow-card zoom">
             <Card.Header>
                 <div className="title">
-                {feedData.title}
+                    {feedData.title}
                 </div>
-                </Card.Header>
+            </Card.Header>
             <Card.Body className="content">
                 <a href={feedData.url} rel="noreferrer" className="regular" target="_blank">
                     {feedData.url}
@@ -102,52 +84,34 @@ function FeedItem({ feedData, editFeed, deleteFeed }) {
             <Card.Footer>
                 <div className="row">
                     <div className="col">
-                        <OverlayTrigger
-                                placement="right"
-                                delay={{ show: 250, hide: 400 }}
-                                overlay={viewTooltip}
-                            >
-                            <Button
-                                variant="primary"
-                                size="sm" block
-                                onClick={viewFeed}>
-                                <FontAwesomeIcon icon={faEye} className="mr-2" />
-                                View
-                            </Button>
-                        </OverlayTrigger>
+                        <Button
+                            variant="primary"
+                            size="sm" block
+                            onClick={viewFeed}>
+                            <FontAwesomeIcon icon={faEye} className="mr-2" />
+                            View
+                        </Button>
                     </div>
                     <div className="col">
-                        <OverlayTrigger
-                            placement="top"
-                            delay={{ show: 250, hide: 400 }}
-                            overlay={editTooltip}
-                        >
-                            <Button
-                                variant="secondary"
-                                size="sm"
-                                onClick={onEditFeed}
-                                block>
-                                    <FontAwesomeIcon icon={faEdit} className="mr-2" />
+                        <Button
+                            variant="secondary"
+                            size="sm"
+                            onClick={onEditFeed}
+                            block>
+                            <FontAwesomeIcon icon={faEdit} className="mr-2" />
                                     Edit
                             </Button>
-                        </OverlayTrigger>
                     </div>
                     <div className="col">
-                        <OverlayTrigger
-                                placement="right"
-                                delay={{ show: 250, hide: 400 }}
-                                overlay={deleteTooltip}
-                            >
-                            <Button
-                                variant="danger"
-                                size="sm"
-                                onClick={onDeleteFeed}
-                                block>
+                        <Button
+                            variant="danger"
+                            size="sm"
+                            onClick={onDeleteFeed}
+                            block>
 
-                                <FontAwesomeIcon icon={faTrash} className="mr-2" />
+                            <FontAwesomeIcon icon={faTrash} className="mr-2" />
                                 Delete
                             </Button>
-                        </OverlayTrigger>
                     </div>
                 </div>
             </Card.Footer>
